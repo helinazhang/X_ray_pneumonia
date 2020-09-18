bb23d3c052070e029321bf47538cd0a231cdbc54
helinazhang <helinazhangzhi@gmail.com>
2020-09-18 09:09:10 +0800

    Add files via upload

---
+++ SGD.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 10:25:35 2020
+
+@author: Jinlong_Dell
+"""
+import os
+import numpy as np
+from tqdm import tqdm
+import pandas as pd 
+
+import cv2
+import matplotlib.pyplot as plt
+import skimage
+
+import numpy as np
+
+
+input_path = 'C:/Users/Jinlong_Dell/Documents/SMU/Sem2/Applied Machine Learning/Project/chest-xray-pneumonia/chest_xray/chest_xray/'
+os.chdir(input_path)
+train_dir = "../train/"
+test_dir =  "../test/"
+
+
+def get_data(folder):
+    X = []
+    y = []
+    for folderName in os.listdir(folder):
+        if not folderName.startswith('.'):
+            if folderName in ['NORMAL']:
+                label = 0
+            elif folderName in ['PNEUMONIA']:
+                label = 1
+            else:
+                label = 2
+            for image_filename in tqdm(os.listdir(folder + folderName)):
+                img_file = cv2.imread(folder + folderName + '/' + image_filename)
+                if img_file is not None:
+                    img_file = skimage.transform.resize(img_file, (150, 150, 1))
+                    #img_file = scipy.misc.imresize(arr=img_file, size=(150, 150, 3))
+                    img_arr = np.asarray(img_file)
+                    X.append(img_arr)
+                    y.append(label)
+    return X,y
+X_train, y_train = get_data(train_dir)
+X_test, y_test= get_data(test_dir)
+
+X_train_rows = np.asarray(X_train)
+X_test_rows = np.asarray(X_test)
+
+# flatten out all images to be one-dimensional
+X_train_rows_flat = X_train_rows.reshape(X_train_rows.shape[0], 150 * 150 * 1) # Xtr_rows becomes 5216 x 67500
+X_test_rows_flat = X_test_rows.reshape(X_test_rows.shape[0], 150 * 150 * 1) # Xte_rows becomes 624 x 67500
+
+import skimage
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.transform import rescale
+
+from sklearn.base import BaseEstimator, TransformerMixin
+ 
+class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
+    """
+    Convert an array of RGB images to grayscale
+    """
+ 
+    def __init__(self):
+        pass
+ 
+    def fit(self, X, y=None):
+        """returns itself"""
+        return self
+ 
+    def transform(self, X, y=None):
+        """perform the transformation and return an array"""
+        return np.array([skimage.color.rgb2gray(img) for img in X])
+ 
+ 
+class HogTransformer(BaseEstimator, TransformerMixin):
+    """
+    Expects an array of 2d arrays (1 channel images)
+    Calculates hog features for each img
+    """
+ 
+    def __init__(self, y=None, orientations=9,
+                 pixels_per_cell=(8, 8),
+                 cells_per_block=(3, 3), block_norm='L2-Hys'):
+        self.y = y
+        self.orientations = orientations
+        self.pixels_per_cell = pixels_per_cell
+        self.cells_per_block = cells_per_block
+        self.block_norm = block_norm
+ 
+    def fit(self, X, y=None):
+        return self
+ 
+    def transform(self, X, y=None):
+ 
+        def local_hog(X):
+            return hog(X,
+                       orientations=self.orientations,
+                       pixels_per_cell=self.pixels_per_cell,
+                       cells_per_block=self.cells_per_block,
+                       block_norm=self.block_norm)
+ 
+        try: # parallel
+            return np.array([local_hog(img) for img in X])
+        except:
+            return np.array([local_hog(img) for img in X])
+        
+        
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_predict
+from sklearn.preprocessing import StandardScaler
+import skimage
+ 
+# create an instance of each transformer
+grayify = RGB2GrayTransformer()
+hogify = HogTransformer(
+    pixels_per_cell=(8, 8),
+    cells_per_block=(2,2),
+    orientations=9,
+    block_norm='L2-Hys'
+)
+
+
+scalify = StandardScaler()
+
+# call fit_transform on each transform converting X_train step by step
+X_train_gray = grayify.fit_transform(X_train_rows)
+X_train_hog = hogify.fit_transform(X_train_gray)
+X_train_prepared = scalify.fit_transform(X_train_hog)
+
+
+
+from imblearn.under_sampling import NearMiss
+
+undersample = NearMiss(version=1, n_neighbors=3)
+
+x_train_rows_us, y = undersample.fit_resample(X_train_prepared, y_train)
+ 
+print(x_train_rows_us.shape)
+
+sgd_clf = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
+sgd_clf.fit(x_train_rows_us, y)
+
+X_test_gray = grayify.transform(X_test_rows)
+X_test_hog = hogify.transform(X_test_gray)
+X_test_prepared = scalify.transform(X_test_hog)
+
+y_pred = sgd_clf.predict(X_test_prepared)
+print(np.array(y_pred == y_test)[:25])
+print('')
+print('Percentage correct: ', 100*np.sum(y_pred == y_test)/len(y_test))
+
+def compute_confusion_matrix(true, pred):
+  '''Computes a confusion matrix using numpy for two np.arrays
+  true and pred.
+
+  Results are identical (and similar in computation time) to: 
+    "from sklearn.metrics import confusion_matrix"
+
+  However, this function avoids the dependency on sklearn.'''
+
+  K = len(np.unique(true)) # Number of classes 
+  result = np.zeros((K, K))
+
+  for i in range(len(true)):
+    result[true[i]][pred[i]] += 1
+
+  return result
+cfm = compute_confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred, labels=[0,1]))
